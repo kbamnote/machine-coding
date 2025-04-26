b00562987a54b6dd881122b96c346f39138bf67d
kbamnote3 <kunalbamnote7@gmail.com>
2025-04-26 23:06:29 +0530

    fifth

--- src/components/tasks/tabSwitch/TabContent.jsx
+++ src/components/tasks/tabSwitch/TabContent.jsx
@@ -11,14 +11,20 @@ setActiveIndex(item)
 
   return (
     <>
+
       <div className="flex justify-center space-x-14 mt-16">
         {tabData.map((item, index) => (
             <div key={index}>
-          <button 
+                <div>
+          <button className={item.label ===activeIndex.label ?'bg-black text-white shadow-lg scale-105 px-4 py-2 w-28 rounded-md font-semibold transition-all duration-300 ':''}
           onClick={()=>handleClick(item)}
           
           >{item.label}</button>
-{item.label===activeIndex.label ? <h1>{item.content}</h1> :<div></div>}
+          </div>
+
+          <div className="mt-12">
+{item.label===activeIndex.label ?<div className=""> <h1 className="">{item.content}</h1></div> :<div></div>}
+</div>
 </div>
         ))}
 
